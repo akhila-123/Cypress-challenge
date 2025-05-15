d31bcbb1aba3fbb9951455126c07a679935ff44e
EU\nlband <akhila.bandiki@miele.com>
2025-05-15 13:49:55 +0200

    feature: add reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);

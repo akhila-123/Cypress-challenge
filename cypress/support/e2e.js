56ab0aa8c2b90edf76987802d8459b2d0e24570c
EU\nlband <akhila.bandiki@miele.com>
2025-05-15 13:41:07 +0200

    feature: add cypress e2e testing workflow - add mochawesome reporter

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,5 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
-import 'cypress-axe'
+import './commands';
+import 'cypress-axe';
+import 'cypress-mochawesome-reporter/register';
